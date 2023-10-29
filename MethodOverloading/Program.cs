dcc1452e1a2c661ddd00b3490fae89395776c35c
macuser301 <macuser30@yahoo.com>
2023-10-29 01:07:41 -0500

    first commit

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = Methods.Add(10,10,true);
+            Console.WriteLine(answer);
+        }
+    }
+}
